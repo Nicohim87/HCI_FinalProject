9e44c03beb648d5eb6b4b8399ae5278ade53fede
Nicohim87 <nico.himawan@binus.ac.id>
2023-05-12 10:38:05 +0700

    Website Done

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" type="image/x-icon" href="./favicon.png">
+    <title>Augmented Reality Room Tour</title>
+</head>
+
+<body>
+    <div id="box">
+
+        <div id="top-text" class="center">
+            <h1>Binus @Malang Augmented Reality Room Tour</h1>
+        </div>
+
+        <div class="center" id="center-element">
+            <b>
+                <div class="button" type="button" onclick="window.location.href = 'https://translate.google.co.id/?sl=en&tl=id&text=Regular%20Class&op=translate';">
+                    <p>Regular Class</p>
+                </div>
+
+                <div class="button" type="button" onclick="window.location.href = 'https://translate.google.co.id/?sl=en&tl=id&text=Creative%20Class&op=translate';">
+                    <p>Creative Class</p>
+                </div>
+
+                <div class="button" type="button" onclick="window.location.href = 'https://translate.google.co.id/?sl=en&tl=id&text=Smart%20Class&op=translate';">
+                    <p>Smart Class</p>
+                </div>
+            </b>
+        </div>
+    </div>
+</body>
+
+</html>
